01911890d162767aee3618f12a75524e07eb46b1
Dheeraj2709 <awsdjdemo@gmail.com>
2023-08-09 15:41:29 +0530

    added jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,17 +20,15 @@ pipeline {
             }
         }
 
-        stage('Publish Extent Report') {
-            steps {
-                publishHTML([allowMissing         : false,
-                             alwaysLinkToLastBuild: false,
-                             keepAll              : true,
-                             reportDir            : 'build',
-                             reportFiles          : 'TestExecutionReport.html',
-                             reportName           : 'HTML Extent Report',
-                             reportTitles         : ''])
-            }
+        stage ('Send Email') {
+
+            mail to: "djmishra2709@gmail.com",
+                    cc: 'manager@domainxxx.com', charset: 'UTF-8',
+                    from: 'Jenkinsnoreply@gmail.com', mimeType: 'text/html', replyTo: '',
+                    bcc: '',
+                    subject: "CI: Project name -> ${env.JOB_NAME}",
+                    body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}"
+        }
 
         }
     }
-}
